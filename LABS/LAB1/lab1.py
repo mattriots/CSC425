8387b67505d2dac39dc5cce83a3702096b0de768
mattriots <mattdepauw@gmail.com>
2023-01-15 11:57:12 -0800

    blah

--- LABS/LAB1/lab1.py
+++ LABS/LAB1/lab1.py
@@ -12,7 +12,7 @@ RC4_KEY = get_random_bytes(RC4_KEY_SIZE)
 
 
 def main():
-    encrypt_AES()
+    AES_ciphertext = encrypt_AES()
     RC4_ciphertext = encrypt_RC4()
     decrypt_RC4(RC4_ciphertext)
 
@@ -27,6 +27,8 @@ def encrypt_AES():
     ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
 
     print ("AES: ", ciphertext)
+
+    return ciphertext
 
 def encrypt_RC4():
 
@@ -43,30 +45,35 @@ def encrypt_RC4():
 
     return ciphertext
 
+def decrypt_AES(ciphertext):
+
+    cipher = AES.new()
+
+
 def decrypt_RC4(ciphertext):
 
     S = list(range(256))
     j = 0
-    out = []
+    output = []
 
     
     listObj = list(S)
     #This works but Im using the KEY. How Can we do it with just the key length!?
+    #I do not believe it is possible. At least for the scope of this lab
 
-    #KSA Phase
     for i in listObj:
-        j = (j + S[i] + RC4_KEY[i % len(RC4_KEY)]) % 256
+        j = (j + S[i] + RC4_KEY[i % RC4_KEY_SIZE]) % 256
         S[i], S[j] = S[j], S[i]
 
-    #PRGA Phase
+
     i = j = 0
     for char in ciphertext:
         i = (i + 1) % 256
         j = (j + S[i]) % 256
         S[i], S[j] = S[j], S[i]
-        out.append(chr(char ^ S[(S[i] + S[j]) % 256]))
+        output.append(chr(char ^ S[(S[i] + S[j]) % 256]))
 
-    decrypted = ''.join(out)
+    decrypted = ''.join(output)
     print(decrypted)
 
 
