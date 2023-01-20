defefdbd01ecda42b698cfe45caf1eb584d85dc8
mattriots <mattdepauw@gmail.com>
2023-01-20 10:48:58 -0800

    test

--- LABS/LAB1/lab1.py
+++ LABS/LAB1/lab1.py
@@ -5,19 +5,20 @@ from Crypto.Util.Padding import pad, unpad
 import random
 import hashlib
 
-hex_val = 'ff' # 1111 1111
+hex_val = 'ff'  # 1111 1111
 
 AES_KEY_SIZE = 16
 AES_KEY = bytes.fromhex(hex_val) * AES_KEY_SIZE
 
 RC4_KEY_SIZE = 5
-RC4_KEY = bytes.fromhex('ff') * RC4_KEY_SIZE
+RC4_KEY = bytes.fromhex(hex_val) * RC4_KEY_SIZE
 
 
 def main():
     AES_ciphertext = encrypt_AES()
     RC4_ciphertext = encrypt_RC4()
     decrypt_RC4(RC4_ciphertext)
+
 
 def encrypt_AES():
 
@@ -29,9 +30,10 @@ def encrypt_AES():
 
     ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
 
-    print ("AES: ", ciphertext)
+    print("AES: ", ciphertext)
 
     return ciphertext
+
 
 def encrypt_RC4():
 
@@ -48,6 +50,7 @@ def encrypt_RC4():
 
     return ciphertext
 
+
 def decrypt_AES(ciphertext):
 
     cipher = AES.new()
@@ -55,19 +58,25 @@ def decrypt_AES(ciphertext):
 
 def decrypt_RC4(ciphertext):
 
+    # Now just need to make loop that increments the key and tests it in the functions below
+    # Then compare it to the ciphertext and see if we have a match?
+
     S = list(range(256))
     j = 0
     output = []
+    test_Key = bytes.fromhex('00') * RC4_KEY_SIZE
+    plain_Key = int.from_bytes(test_Key, 'big')
+    plain_Key = plain_Key + 1
 
-    
+    test_Key = plain_Key.to_bytes(RC4_KEY_SIZE, 'big')
+
     listObj = list(S)
-    #This works but Im using the KEY. How Can we do it with just the key length!?
-    #I do not believe it is possible. At least for the scope of this lab
+    # This works but Im using the KEY. How Can we do it with just the key length!?
+    # I do not believe it is possible. At least for the scope of this lab
 
     for i in listObj:
-        j = (j + S[i] + RC4_KEY[i % RC4_KEY_SIZE]) % 256
+        j = (j + S[i] + test_Key[i % RC4_KEY_SIZE]) % 256
         S[i], S[j] = S[j], S[i]
-
 
     i = j = 0
     for char in ciphertext:
@@ -81,5 +90,3 @@ def decrypt_RC4(ciphertext):
 
 
 main()
-
-
